a418fa80a359ac08129a2ef736fd442962b71607
Millena <millenapoprock@gmail.com>
2024-03-28 21:52:37 -0300

    inicio

---
+++ backend/src/main/java/com/loja/backend/controller/hello.java
@@ -0,0 +1,18 @@
+package com.loja.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+@RestController
+@RequestMapping ("/api")
+public class hello {
+    @GetMapping("/")
+
+    public String Hello(){
+        return "Hello Millena" +new Date();
+
+    }
+    
+}
